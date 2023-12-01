fbb86095d078e7f18db2ee0b6a280fde3d5360aa
Paul <pauldav61@gmail.com>
2023-12-01 17:57:28 +0100

    Subiendo archivos de proyecto

---
+++ 1-Date_clear.py
@@ -0,0 +1,246 @@
+import os 
+import pandas as pd
+import numpy as np
+import time
+from progress.bar import Bar
+###----------------------------------------TIEMPO TOTAL DE EJECUCION: 3.5 MINUTOS APROX -----------------------------------###
+
+###--------------------------------------------Tarea 1: Carga de datos-----------------------------------------------------###
+
+# 0-Se creado un work space en vscode para poder trabajar
+directory = os.getcwd()
+NOTA_0 = "Se ha agregado el paquete progress, dado que la ejecución puede tardar mas de 1 minuto."
+
+# 1-Leyendo el archivo csv y cargandolo con el nombre df en pandas
+df = pd.read_csv('Airbnb_Open_Data.csv')
+
+# 2-Visualiza las cinco primeras filas
+Total_inicial_filas=len(df)
+print(df.head(5))
+
+# 3-Diferentes formas de mirar las propiedades del df.
+print(df.dtypes)
+print(df.info())
+print(df.columns)
+print("\n")
+
+###------------------------------------------Tarea 2a: Limpieza de datos--------------------------------------------------------###
+
+# 1-Eliminando las columnas no deseadas para realizar el análisis.
+df.drop([
+        'NAME', 'host id', 'host name', "country", 'country code', 'last review', 'house_rules',
+         "number of reviews", "reviews per month"
+        ], axis=1, inplace=True)
+
+# 2-Razones por la cual se han eliminado estas columnas. 
+Rpta_1="NAME, host id, host name : son campos mas personalizados, que permiten realizar seguimientos mas individuales ( este no es el caso)."
+Rpta_2="Country, country code : información redundante, dado que los datos pertenecen al mismo país."
+Rpta_3="house_rules: información no clasificable para un tratamiento estadístico"
+Rpta_4="last review, number of review y reviews per month: estos campos estas englobados en el parámetro review rate number, que es el indicador."
+Razon_columnas_eliminadas = {'NAME':Rpta_1, 'host id':Rpta_1, 'host name':Rpta_1, "country":Rpta_2, 'country code':Rpta_2,
+                            'last review':Rpta_4,'house_rules':Rpta_3,"number of reviews":Rpta_4, "reviews per month":Rpta_4
+                            }
+def Consultar_columna_eliminada(campo): #Función para ingresar nombre de la columna y devolver el motivo por el cual se elimino esta. 
+    Motivo_delete=Razon_columnas_eliminadas.get(campo)
+    print("Se elimina la columna/s por el siguiente motivo:")
+    print(Motivo_delete)
+Consultar_columna_eliminada("house_rules")
+
+# 3-Corrigiendo nombres de neighbourhood_group  que estan mal escritos: brookln/manhatan, remplazandolos por Brooklyn/Manhattan
+df['neighbourhood group'] = df['neighbourhood group'].replace({"brookln": "Brooklyn", 'manhatan': "Manhattan"})
+NOTA_1= "Al existir menos de 250 vecindarios, se ha llevado a cabo la agrupación de vecindarios y la revisión visual, observando que no existen vecindarios mal escritos."
+print("\n")
+
+###------------------------------------------Tarea 2b: Limpieza de datos-----------------------------------------------------###
+
+# 1-Valores nulos mostrados de forma ascendente.
+Valores_nulos=df.isnull().sum().sort_values(ascending=True)
+print(Valores_nulos)
+
+for col in df.columns: # Otra forma de comprobar los valores nulos
+    Val_Null= df[col].isnull().sum()
+    print("La columna " + col + " tiene : " + str(Val_Null) + " valores nulos.")
+
+# 2-Comprobando filas duplicadas y eliminandolas.
+Total_filas_duplicadas=df.duplicated().sum() 
+print("Existen " + str(Total_filas_duplicadas) + "filas duplicadas")
+
+df.drop_duplicates(inplace=True) 
+Total_filas_elim=df.duplicated().sum()
+print("El total de filas duplicadas es : " + str(Total_filas_elim))
+
+# 3-Reseteamos la columna de indices para futuros calculos
+df.reset_index(inplace=True, drop=True) 
+
+# 4-Muestra el número total de registros antes y después de eliminar los duplicados.
+Total_filas_sin_duplicados=len(df)
+print   ("Filas iniciales - filas finales es " + str(Total_inicial_filas)+"-"+str(Total_filas_sin_duplicados) \
+        + "=" +str(Total_inicial_filas-Total_filas_sin_duplicados) \
+        + " , debe ser igual a "  + str(Total_filas_duplicadas) + " filas duplicadas"
+        )
+print("\n")
+
+###------------------------------------------Tarea 2c: Limpieza de datos-------------------------------------------------------------###
+
+# OBSERVACIÓN IMPORTANTE:
+NOTA_2="Se ha llenado los campos nulos con No Data, ya que al no disponer de la información, preferimos realizar la estadística descriptiva \
+                con los datos reales, ya que el remplazo de valores nulos por medias, modas, etc, puede llevar a conclusiones equivocas."
+NOTA_3="En el momento de reaizar los calculos, se omitira el valor No Data en las columnas con variables númericas, y en algún caso en las \
+                columnas con variables categoricas, se podría mostrar para ver la cantidad de campos no registrados."
+
+# 1-Rellenando los NULL de la columna review rate number con el valor de cero, ya que es el único campo que se puede poner cero en lugar de Null.
+df['review rate number'].fillna(0, inplace=True) 
+
+# 2-Rellenando los NULL de la columna instant_bookable con False, asumimos nulos como falsos al carecer de información confirmada.
+df['instant_bookable'].fillna("False", inplace=True) 
+
+# 3-Rellenando los NULL de la columna host_identity_verified con unconfirmed , ya que asumimos que los nulos son no confirmados.
+df['host_identity_verified'].fillna("unconfirmed", inplace=True) 
+
+# 4-Cambiando campos booleanos por string
+def bollean_string(columna):
+    bar = Bar('Processing', max=len(df))
+    for j in range(0,len(df)):
+        valor_st=str(df[columna][j])
+        df[columna][j]=valor_st
+        bar.next()
+    bar.finish()
+inicio_0=time.time()
+print(".......................Espere!!!!!......................, convirtiendo valores a strings.")
+bollean_string("instant_bookable")
+Fin_0=time.time()
+tiempor_total_0=Fin_0-inicio_0
+print(" Tipo de datos cambiado, tiempo de la ejecución : " + str(round((tiempor_total_0/60),2)) + " minutos.") 
+
+# 5-Rellenando todos los nulos  del df con "No Data"
+df.fillna("No Data", inplace=True)
+
+# 6-Comprobando que no existan nulos en el df
+Valores_nulos_final=df.isnull().sum().sort_values(ascending=True)
+print(Valores_nulos_final)
+print("\n")
+
+###------------------------------------------Tarea 3: Transformación de datos-----------------------------------------------------###
+
+# 1-Cambiando el nombre de la columna `availability 365` a `days_booked`.
+df.rename(columns={'availability 365': 'days_booked'}, inplace=True)
+for columna in df:
+    if columna == "days_booked":
+        print("Nuevo nombre de la columna availability_365 es days booked")
+
+# 2-Convirtiendo todos los nombres de las columnas a minúsculas y sustituyendo los espacios por un guión bajo "_".
+df.columns = [col.lower().replace(" ", "_") for col in df.columns]
+print(df.columns)
+
+# 3-Eliminando el signo $ y la coma de las columnas `price` y `service_fee`. Si es necesario, convierte estas dos columnas al tipo de datos adecuado.
+NOTA_4="OBSERVACIÓN IMPORTANTE: dado que la columna es un objeto y existen datos númericos y strings, usamos el try para realizar el bucle y saltar los errores."
+def remove_dollar_sign(columna):
+    bar = Bar('Processing', max=len(df))
+    for i in range (0,len(df)):
+        try:
+            a=df[columna][i].replace("$", "")
+            df.loc[i, columna] = a
+        except:
+            pass
+        try:
+            a=df[columna][i].replace(",", "")
+            df.loc[i, columna] = a
+        except:
+            pass
+        bar.next()
+    bar.finish()
+inicio=time.time()
+print(".......................Espere!!!!!...............,removiendo signo $ y espacios en blanco en 2 columnas.")
+remove_dollar_sign("price")
+remove_dollar_sign("service_fee")
+Fin=time.time()
+tiempor_total=Fin-inicio
+print("$ y espacios removidos, tiempo de la ejecución : " + str(round((tiempor_total/60),2)) + " minutos.")
+
+Dicc_col_df={
+            "price":1, "service_fee":1, "lat":1, "long":1, "construction_year":2, "minimum_nights":2,
+            "days_booked":2, "calculated_host_listings_count":2, "review_rate_number":2
+            }
+def convert_col(columna, opcion):
+    bar = Bar('Processing', max=len(df))
+    for i in range (0,len(df)):
+        if opcion == 1:
+            try:
+                a=float(df[columna][i])
+                df.loc[i, columna] = a
+            except:
+                pass
+        if opcion == 2:
+            try:
+                a=int(df[columna][i])
+                df.loc[i, columna] = a
+            except:
+                pass
+        bar.next()
+    bar.finish()   
+inicio_2=time.time()
+print(".......................Espere!!!!!.................,convirtiendo campo a float/int, en 9 columnas.")
+
+for clave, valor in Dicc_col_df.items():
+    convert_col(clave, valor)
+Fin_2=time.time()
+tiempor_total_2=Fin_2-inicio_2
+print("Campos cambiados a int/float, tiempo de la ejecución : " + str(round((tiempor_total_2/60),2)) + " minutos.")
+
+# 4-Cambiando numeros negativos por 1 en las columnas minimun_nights y days_booked.
+print(".......................Espere!!!!!......................, cambiando negativos por unos en columna minimun_nights y days_booked.")
+def remove_boolean(columna1, columna2):
+    bar = Bar('Processing', max=len(df))
+    for i in range (0,len(df)):
+        try:
+            b=df[columna1][i]
+            if b < 0:
+                df.loc[i, columna1] = 1
+        except:
+            pass
+        try:
+            d=df[columna2][i]
+            if d < 0:
+                df.loc[i, columna2] = 1
+        except:
+            pass
+        bar.next()
+    bar.finish()
+inicio_3=time.time()
+remove_boolean("minimum_nights", "days_booked")
+Fin_3=time.time()
+tiempor_total_3=Fin_3-inicio_3
+print(" Tipo de datos cambiado, tiempo de la ejecución : " + str(round((tiempor_total_3/60),2)) + " minutos.") 
+print("\n")
+
+###------------------------------------------Tarea 4: Busqueda de datos faltantes-----------------------------------------------------###
+
+# 1-Completando campos nulos de la columna nieghbourhood group con datos de otros registros correspondientes al mismo vecindario, lo contrario no se puede hacer.
+lista_neighbourhood=df["neighbourhood"].unique()
+vecindario_grupoVecindarios={}
+def dicc_neight():
+    for m in lista_neighbourhood:
+        for n in range(0, len(df)):
+            com1 = df["neighbourhood"][n]
+            com2 = df["neighbourhood_group"][n]
+            if com1 != "No Data" and com2 != "No Data":
+                vecindario_grupoVecindarios[com1] = com2
+        break 
+def agregar_valores():
+    for z in range (len(df)):
+        if (df["neighbourhood_group"][z] == "No Data") and (df["neighbourhood"][z] != "No Data") :
+            val_buscar= df["neighbourhood"][z]
+            remplazo=vecindario_grupoVecindarios[val_buscar]
+            df.loc[z, 'neighbourhood_group'] = remplazo
+dicc_neight()
+agregar_valores()
+print("\n")
+###----------------------- Guardamos el df como un csv modificado para realizar el tratamiento estadístico-------------------------###
+
+print("Espere!!!!!...... exportando df como archivo .csv y .xlsx")
+df.to_csv("2-Final_Proyect_modificado.csv")
+df.to_excel("3-Final_Proyect_modificado.xlsx", index=False)
+print("df exportado como archivo csv y xlsx.")
+
+
+
---
+++ 4-data_treatment.py
@@ -0,0 +1,192 @@
+import os 
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+from progress.bar import Bar
+
+# importamos nuestro nuevo csv modificado desde la carpeta de trabajo de vscode
+df_calc = pd.read_csv('2-Final_Proyect_modificado.csv') 
+
+# Convertimos los datos de la columna instant_bookable a strings.
+def bollean_string(columna):
+    bar = Bar('Processing', max=len(df_calc))
+    for j in range(0,len(df_calc)):
+        valor_st=str(df_calc[columna][j])
+        df_calc[columna][j]=valor_st
+        bar.next()
+    bar.finish()
+print(".......................Espere!!!!!......................, convirtiendo valores a strings.")
+bollean_string("instant_bookable")
+print(" Tipo de datos cambiado.")
+print("\n") 
+
+###------------------------------------------Tarea 4: Análisis exploratorio de datos ------------------------------------------###
+
+# 1-Tipos de habitaciones disponibles en Airbnb.
+lista_tipos_habitacion=df_calc["room_type"].unique() 
+print("Tipos de habitación disponible :")
+print(lista_tipos_habitacion)
+Tipos_habitacion=df_calc['room_type'].value_counts()
+print(Tipos_habitacion)
+
+# 2-Tipo de habitación que se adhiere a una política de cancelación más estricta.
+df_room_type_strict=df_calc[df_calc["cancellation_policy"]=="strict"]
+tipo_hab_strict= df_room_type_strict['room_type'].value_counts().sort_values(ascending=False)
+print("Tipos de habitación con política estricta (nº total por tipo strict) : la primera fila es el room_type que se adhiere a una política de cancelación mas estricta")
+print(tipo_hab_strict)
+
+# 3-Precios por barrio.
+df_grlp=df_calc[['neighbourhood', 'price']][df_calc['price'] != "No Data"]
+df_grlp["price"] = df_grlp["price"].astype(str).astype(float)
+Neighbourhood_prices = df_grlp.groupby('neighbourhood').mean().sort_values(by="neighbourhood", ascending=True)
+print("Agrupado de precios por barrio ordenados de forma alfabética :")
+for index, row in Neighbourhood_prices.iterrows():
+    vale=round(Neighbourhood_prices.iloc[Neighbourhood_prices.index.get_loc(index), 0],2)
+    print(index,vale)   
+
+# 4-grupo de barrios con los alquileres mas caros.
+df_grlp_2=df_calc[['neighbourhood_group', 'price']][(df_calc['neighbourhood_group'] != "No Data") & (df_calc['price'] != "No Data") ]
+df_grlp_2["price"] = df_grlp_2["price"].astype(str).astype(float)
+Neighbourhood_grouped_prices = df_grlp_2.groupby('neighbourhood_group').mean().sort_values(by="price", ascending=False)
+print("Grupo de vecindarios ordenados por la media de precios, el grupo con los alquileres mas caros es:")
+print(Neighbourhood_grouped_prices.head(1))
+print("\n")
+
+###--------------------------------------- Tarea 5a: Visualización de datos--------------------------------------------------------###
+
+# 1-Los 10 barrios más caros (precio ascendente)(gráfico de barras horizontales).
+Neighbourhood_prices_ascending = df_grlp.groupby('neighbourhood').mean().sort_values(by="price", ascending=False)
+print("El barrio mas barato es :")
+print(Neighbourhood_prices_ascending[-1:])
+Neighbourhood_prices_top10=Neighbourhood_prices_ascending[:10].sort_values(by="price", ascending=True)
+
+plt.figure(figsize=(14,6), dpi=80)
+sns.barplot(data=Neighbourhood_prices_top10, x='price', y='neighbourhood', errorbar=None, palette="Blues_d") 
+plt.xticks(rotation=45)
+plt.yticks(rotation=35)
+plt.xlabel("Precio del alquiler ($)")
+plt.ylabel("Vecindarios de EEUU")
+plt.title("10 Barrios con el alquiler de habitación mas caro según la media de precios", fontdict = {'fontsize': 14,'fontweight': 'bold', 'color': '#5A1FCC'})
+plt.show()
+print("\n")
+
+# 2-Barrios que ofrecen alquileres a corto plazo de menos de 10 días (gráfico de barras verticales).
+    # Suponemos que el corto plazo viene determinado por instant_bookable y los días por minimum_nights
+df_grlp_3=df_calc[['neighbourhood', 'instant_bookable', 'minimum_nights']] [(df_calc['neighbourhood'] != "No Data") &
+                                                                            (df_calc['instant_bookable'] == "True") &
+                                                                            (df_calc['minimum_nights'] != "No Data")
+                                                                            ]
+df_grlp_3["minimum_nights"] = df_grlp_3["minimum_nights"].astype(str).astype(int)
+df_grlp_3=df_grlp_3[['neighbourhood', 'minimum_nights']][df_grlp_3['minimum_nights']<10]
+alquileres_corto_plazo = df_grlp_3.groupby('neighbourhood').count().sort_values(by="minimum_nights", ascending=False).head(25)
+print(alquileres_corto_plazo)
+plt.rcParams.update({'font.size': 6})
+alquileres_corto_plazo.plot(kind= 'bar', figsize=(14,6))
+plt.xlabel('Vecindario')
+plt.ylabel('nº de habitaciones')
+plt.xticks(rotation=90)
+#plt.yticks(rotation=30)
+plt.title('Alquileres por vecindario con reserva instantanea y mínimo de noches menor a 10 días', fontdict = {'fontsize': 12,'fontweight': 'bold', 'color': '#5A1FCC'})
+plt.show()
+
+# 3-Precios con respecto al tipo de habitación (gráfico de barras).
+df_grlp_4=df_calc[['room_type', 'price']][df_calc['price'] != "No Data"]
+df_grlp_4["price"] = df_grlp_4["price"].astype(str).astype(float)
+precios_tipo_habitacion = df_grlp_4.groupby('room_type').mean().sort_values(by="price", ascending=True)
+print(precios_tipo_habitacion)
+
+plt.figure(figsize=(10,6), dpi=80)
+sns.barplot(data=precios_tipo_habitacion, x='room_type', y='price', errorbar=None, hue="room_type") 
+#plt.xticks(rotation=45)
+#plt.yticks(rotation=35)
+plt.xlabel('Tipo de habitación')
+plt.ylabel('Media de precios ($)')
+plt.title('Media de precios de alquiler de habitación según su tipo', fontdict = {'fontsize': 14,'fontweight': 'bold', 'color': '#5A1FCC'})
+plt.show()
+print("\n")
+
+# 4-Distribución de los días reservados para cada grupo de barrios (gráfico circular).
+df_grlp_5=df_calc[['neighbourhood_group', 'days_booked']][(df_calc['neighbourhood_group'] != "No Data") &
+                                                                            (df_calc['days_booked'] != "No Data")  
+                                                                            ]
+df_grlp_5["days_booked"] = df_grlp_5["days_booked"].astype(str).astype(int)
+DiasLibres_grupo_vec = df_grlp_5.groupby('neighbourhood_group').sum().sort_values(by="days_booked", ascending=True)
+Lista_ag_vec=[]
+Lista_ag_days=[]
+for index, row in DiasLibres_grupo_vec.iterrows():
+    val=DiasLibres_grupo_vec.iloc[DiasLibres_grupo_vec.index.get_loc(index), 0]
+    Lista_ag_vec.append(index)
+    Lista_ag_days.append(val)
+
+Total_dias_libres = Lista_ag_days
+Grupo_vecindario = Lista_ag_vec
+plt.figure(figsize=(8, 6))
+plt.pie(Total_dias_libres, labels=Grupo_vecindario, autopct="%0.1f %%")
+plt.axis("equal")
+plt.title("Distribución anual del total de los días reservados por grupo de barrio", fontdict = {'fontsize': 14,'fontweight': 'bold', 'color': '#5A1FCC'})
+plt.show()
+print("\n")
+
+#-------------------------------------------------Tarea 5b: Visualización de datos-----------------------------------------------------#
+
+# 1-Impacto entre el precio del servicio y el precio de la habitación (gráfico de dispersión).
+df_grlp_6=df_calc[['room_type','service_fee', 'price']][df_calc['price'] != "No Data"][df_calc['service_fee'] != "No Data"]
+df_grlp_6["price"] = df_grlp_6["price"].astype(str).astype(float)
+df_grlp_6["service_fee"] = df_grlp_6["service_fee"].astype(str).astype(float)
+
+plt.figure(figsize=(12, 6))
+sns.scatterplot(data=df_grlp_6, x='service_fee', y='price', hue='room_type', s=36)
+plt.xlabel('Tarifa de limpieza ($)')
+plt.ylabel('Precio de la habitación ($)')
+plt.title('Relación entre la tarifa de limpieza y el precio de la habitación', fontdict = {'fontsize': 14,'fontweight': 'bold', 'color': '#5A1FCC'}) 
+plt.show()
+
+correlation = df_grlp_6['service_fee'].corr(df_grlp_6['price'])
+print('Coeficiente de correlación: ', correlation)
+
+# 2-Distribución de construcción de habitaciones por año (gráfico de líneas).
+# cada habitación tiene asignada una id única, ya que un mismo lugar tiene varia habitaciones.
+df_grlp_7=df_calc[['construction_year', 'id']][df_calc['construction_year'] != "No Data"]
+Habitaciones_x_agno = df_grlp_7.groupby('construction_year').count().sort_values(by="construction_year")
+
+plt.figure(figsize=(12, 6))
+sns.lineplot(data=Habitaciones_x_agno)
+plt.xlabel('Año de construcción')
+plt.ylabel('Habitaciones construidas')
+plt.title('Cantidad total de habitaciones habilitadas por año', fontdict = {'fontsize': 14,'fontweight': 'bold', 'color': '#5A1FCC'})
+plt.xticks(rotation=35)
+plt.show()
+print("\n")
+
+###-----------------------------------------------Tarea 5c: Visualización de datos-------------------------------------------------###
+
+# 1-Efecto del número de tasa de revisión en el precio (gráfico de caja).
+df_grlp_8=df_calc[['review_rate_number', 'price']][df_calc['price'] != "No Data"]
+df_grlp_8["price"] = df_grlp_8["price"].astype(str).astype(float)
+plt.figure(figsize=(8, 6))
+sns.boxplot(x='review_rate_number', y='price', data=df_grlp_8)
+plt.xlabel('review_rate_number')
+plt.ylabel('Precio del alquiler de habitación ($)')
+plt.title('Efecto del número de tasa de revisión en el precio', fontdict = {'fontsize': 14,'fontweight': 'bold', 'color': '#5A1FCC'})
+plt.show()
+
+# 2-Efecto de la identidad del host verificada en el precio (gráfico de caja).
+df_grlp_9=df_calc[['host_identity_verified', 'price']][df_calc['price'] != "No Data"]
+df_grlp_9["price"] = df_grlp_9["price"].astype(str).astype(float)
+
+plt.figure(figsize=(8, 6))
+sns.boxplot(x='host_identity_verified', y='price', data=df_grlp_9)
+plt.xlabel('Identidad del anfitrion')
+plt.ylabel('Precio del alquiler de la habitación ($)')
+plt.title('Efecto de la verificación de identidad del anfitrión en el precio', fontdict = {'fontsize': 12,'fontweight': 'bold', 'color': '#5A1FCC'})
+plt.show()
+
+# 3-Calculando los cuartiles.
+df_grlp_10=df_calc[['host_identity_verified', 'price']][df_calc['price'] != "No Data"][df_calc['host_identity_verified'] != "verified"]
+df_grlp_11=df_calc[['host_identity_verified', 'price']][df_calc['price'] != "No Data"][df_calc['host_identity_verified'] != "unconfirmed"]
+df_grlp_10["price"] = df_grlp_10["price"].astype(str).astype(float)
+df_grlp_11["price"] = df_grlp_11["price"].astype(str).astype(float)
+
+print(df_grlp_10["price"].quantile([.25, .5, .75, 1]))
+print(df_grlp_11["price"].quantile([.25, .5, .75, 1]))
